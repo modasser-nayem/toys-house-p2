2f71cbba3dbe171fcc69c456b09eeb77f2bcd1cc
developer-nayem <mdalimodassernayem@gmail.com>
2023-05-18 15:07:06 +0600

    implement register user

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -146,7 +146,7 @@ const Navbar = () => {
                            </div>
                         ) : (
                            <Link
-                              className="tooltip tooltip-left tooltip-warning text-white"
+                              className="tooltip tooltip-left text-white"
                               data-tip="Login"
                               to="/login"
                            >
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useState } from "react";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 
 export const AuthContext = createContext();
 
@@ -6,12 +7,31 @@ const AuthProvider = ({ children }) => {
    const [user, setUser] = useState(null);
    const [loading, setLoading] = useState(false);
 
+   const createNewUser = (email, password, name, photo_url, navigate) => {
+      createUserWithEmailAndPassword(auth, email, password)
+         .then((userCredentials) => {
+            updateUserProfile(userCredentials.user, name, photo_url);
+            logoutUser("");
+            toast.success("User Created Successfully");
+            navigate("/login");
+         })
+         .catch((error) => {
+            toast.error(error.message.slice(22, 42));
+         });
+   };
+
+   const updateUserProfile = (user, name, photo_url) => {
+      updateProfile(user, { displayName: name, photoURL: photo_url });
+   };
+
    const info = {
       // user: {
       //    displayName: "Nayem",
       //    email: "alimodasser@gmail.com",
       //    photoURL: "sssssssffffffffffff",
       // },
+      createNewUser,
+      updateUserProfile,
       user,
       loading,
    };
